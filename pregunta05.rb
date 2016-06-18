29674fe22e1e7506928c5ab0453c9f4bcf085627
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-06-17 19:52:26 -0500

    Lista pregunta 5

---
+++ pregunta05.rb
@@ -0,0 +1,12 @@
+# Se tienen dos arreglos
+# El primero es del tipo [1,2,3,0,1,2,2,1,2,1,2,0,3] El segundo es del tipo [:azul, :verde, :amarillo]
+# Se pide cambiar todas las apariciones del número que aparece en el arreglo 1 por el elemento con el mismo índice del arreglo 2, en caso de no existir el elemento deberá ser remplazado por nil.
+# El resultado de este ejercicio debería quedar:
+# [:verde, :amarillo, nil, :azul, :verde, :amarillo ….]
+
+# Dado los siguientes arreglos:
+
+numeros = [1,2,3,0,1,2,2,1,2,1,2,0,3]
+colores = [:azul, :verde, :amarillo]
+
+print numeros.map { |e| colores[e]  }
