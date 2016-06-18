bb8d19f0ddc18e2c77ca214cc21f3dfed0ea3d1a
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-06-17 19:41:03 -0500

    Lista pregunta 3

--- pregunta03.rb
+++ pregunta03.rb
@@ -7,8 +7,10 @@ c = ["Tacos", "Quesadillas", "Hamburguesas"]
 # Iterando los arreglos anteriroes crear un único arreglo final con:
 # [1, :azul, Tacos, 2, :rojo, Quesadillas, 3, :amarillo, “Hamburguesas]
 
+fin = a.length-1
+
 final = []
-for i in 0..2
+for i in 0..fin
   final.push(a[i])
   final.push(b[i])
   final.push(c[i])
