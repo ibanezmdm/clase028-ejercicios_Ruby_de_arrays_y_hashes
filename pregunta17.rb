27e550f80020ad73244621a7e179657af117961d
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-06-19 19:58:51 -0500

    Lista pregunta 17

---
+++ pregunta17.rb
@@ -0,0 +1,14 @@
+# Regiones y ciudades.
+# 1. create a mapping of regions to abbreviation
+# 2. create a basic set of regions and some cities in them
+# 3. add some more cities
+# 4. puts out some cities
+# 5. puts some regions
+# 6. do it by using the regions then cities dict
+# 7. puts every region abbreviation
+# 8. puts every city in regions
+# 9. now do both at the same time
+# 10. by default ruby says "nil" when something isn’t in there
+# 11. default values using ||= with the nil result
+
+require "pp"
