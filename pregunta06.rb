096aa935b14032d5189a9c55ea70fb3d4b9756d8
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-06-20 17:37:44 -0500

    Optimizando la Pregunta 06

--- pregunta06.rb
+++ pregunta06.rb
@@ -1,9 +1,9 @@
 # Generar 20 números enteros al azar y guardarlos dentro de un arreglo, mostrar el arreglo y luego mostrar del arreglo exclusivamente aquellos números que sean mayor que 10
 # http://ruby-doc.org/core-2.2.0/Random.html
 
-numeros = []
-20.times do
-  numeros.push(rand(20))
-end
+require 'pp'
 
-print numeros.select{ |e| e > 10}
+numeros = []
+20.times{numeros << rand(20)}
+
+pp numeros.select{ |e| e > 10}
