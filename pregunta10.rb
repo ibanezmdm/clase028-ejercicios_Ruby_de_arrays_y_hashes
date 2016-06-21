f7ace6ae686dc7fb963dd0f55e977678463c9f5c
ibanezmdm <ibanez.am@gmail.com>
2016-06-21 11:54:21 -0300

    Update pregunta10.rb

--- pregunta10.rb
+++ pregunta10.rb
@@ -16,3 +16,8 @@ result = []
 arr.each{ |e| result << suma_array(e) if e.class == Array}
 
 pp result
+
+# Sebastian Cornejo
+# Yo hice esto
+# a =  [1,2,3,[1,2,3],4,5,6,[1,2,3]]
+# r = a.select { |e| e.class == Array }.map { |e| e.inject { |sum, x| sum += x} }
